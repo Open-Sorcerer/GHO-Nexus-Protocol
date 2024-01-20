43b4fc5e326bb42aa9003be09c9a26be929fac8d
Saviour1001 <harshghodkar@gmail.com>
2024-01-21 02:41:26 +0530

    integrated the smart contract

--- src/lib/smartContractUtils.ts
+++ src/lib/smartContractUtils.ts
@@ -1,22 +1,115 @@
+import GHOTokenABI from './GHOTokenABI.json'
 import tokenBridgeABI from './tokenBridgeABI.json'
-import { tokenBridgeEthSepoliaAddress } from './consts'
+import { tokenBridgeEthSepoliaAddress, tokenBridgeArbSepoliaAddress } from './consts'
 
-import { getContract } from 'viem'
 import { sepoliaPublicClient } from './clients'
-import { useAccount, useWalletClient } from 'wagmi'
 import { simulateContract } from 'viem/_types/actions/public/simulateContract'
+import {
+	useAccount,
+	useWalletClient,
+	usePublicClient,
+	usePrepareContractWrite,
+	useContractRead,
+	useContractWrite,
+} from 'wagmi'
+
+import { formatEther, parseEther } from 'viem'
+import {
+	chainIdArbitrumSepolia,
+	chainIdEthereumSepolia,
+	GhoTokenAddressEthSepolia,
+	GhoTokenAddressArbSepolia,
+} from './consts'
 
 const useBridge = () => {
 	const { address } = useAccount()
 	const walletClient = useWalletClient()
+	const publicClient = usePublicClient()
+
+	/// first call GHO Token Address
+	/// Call the function allowance to get the amount of tokens that the spender can spend from the owner
+	//
+	const { data: GhoArbSepoliaAllowance } = useContractRead({
+		address: GhoTokenAddressArbSepolia,
+		abi: GHOTokenABI,
+		functionName: 'allowance',
+		args: [address, tokenBridgeArbSepoliaAddress],
+	})
+
+	const { data: GhoEthSepoliaAllowance } = useContractRead({
+		address: GhoTokenAddressEthSepolia,
+		abi: GHOTokenABI,
+		functionName: 'allowance',
+		args: [address, tokenBridgeEthSepoliaAddress],
+	})
+
+	/// Allowance Address 1 = Owner Addre2 = Spender
+
+	/// If allowed amount is less than the amount you want to transfer, call the function approve to allow the spender to spend the amount you want to transfer
+	/// approval address = spender and amount = amount you want to transfer
+
+	const { config: GhoArbSepoliaApprovalConfig } = usePrepareContractWrite({
+		address: GhoTokenAddressArbSepolia,
+		abi: GHOTokenABI,
+		functionName: 'approve',
+		args: [
+			tokenBridgeArbSepoliaAddress, // giving spending rights to the bridge
+			2000000000000000000n,
+		], // 1 GHO
+	})
+
+	const { write: GhoArbSepoliaApproval } = useContractWrite(GhoArbSepoliaApprovalConfig)
+
+	const { config: GhoEthSepoliaApprovalConfig } = usePrepareContractWrite({
+		address: GhoTokenAddressEthSepolia,
+		abi: GHOTokenABI,
+		functionName: 'approve',
+		args: [
+			tokenBridgeEthSepoliaAddress, // giving spending rights to the bridge
+			2000000000000000000n, // 1 GHO
+		], // 1 GHO, change to variable
+	})
+
+	const { write: GhoEthSepoliaApproval } = useContractWrite(GhoEthSepoliaApprovalConfig)
+
+	/// Call Bridge function
+
+	const { config: GhoArbSepoliaBridgeConfig } = usePrepareContractWrite({
+		address: tokenBridgeArbSepoliaAddress,
+		abi: tokenBridgeABI,
+		functionName: 'transferTokens',
+		args: [
+			chainIdEthereumSepolia, // sending to Ethereum Sepolia
+			address, // receiver address
+			GhoTokenAddressArbSepolia, // source chain token address (Arbitrum Sepolia)
+			1000000000000000000n, // amount
+		], // 1 GHO, change to variable
+	})
+
+	const { write: GhoArbSepoliaBridge, data } = useContractWrite(GhoArbSepoliaBridgeConfig)
+
+	const { config: GhoEthSepoliaBridgeConfig } = usePrepareContractWrite({
+		address: tokenBridgeEthSepoliaAddress,
+		abi: tokenBridgeABI,
+		functionName: 'transferTokens',
+		args: [
+			chainIdArbitrumSepolia, // sending to Arbitrum Sepolia
+			address, // receiver address
+			GhoTokenAddressEthSepolia, // source chain token address (Ethereum Sepolia)
+			1000000000000000000n, // amount
+		], // 1 GHO, change to variable
+	})
+
+	const { write: GhoEthSepoliaBridge } = useContractWrite(GhoEthSepoliaBridgeConfig)
 
 	const callBridge = async () => {
-		console.log('hello')
-	}
+		console.log('publicClient', await publicClient.getChainId())
+		console.log('EthSepoliaData', formatEther(GhoEthSepoliaAllowance as bigint))
 
-	return {
-		callBridge,
+		// GhoEthSepoliaApproval?.()
+		GhoEthSepoliaBridge?.()
 	}
+	return { callBridge }
 }
 
 export default useBridge
